b7b2ee64f7e9b34c583ec87eb4607b18a846cb89
rich98521 <richie98521@gmail.com>
2015-12-16 22:33:53 +0000

    RIC-4, RIC-5 Add enemy path following, pathfinding

--- TowerD/Enemy.cpp
+++ TowerD/Enemy.cpp
@@ -16,9 +16,56 @@ void Enemy::LoadAssets()
 	Entity::LoadAssets();
 }
 
-void Enemy::Update(float t, sf::Vector2f core)
+void Enemy::Update(float t, sf::Vector2f offset, float scale)
 {
-	float ang = atan2(core.y - mLocation.y, core.x - mLocation.x);
-	mAccel = sf::Vector2f(cos(ang), sin(ang)) * 100.f;
-	Entity::Update(t);
+	if (!mGoalReached)
+	{
+		sf::Vector2f target(mPath[currentNode].first.x * mTileSize, mPath[currentNode].first.y * mTileSize);
+		if (ManhDist(mLocation.x, mLocation.y, target.x, target.y) < 80)
+		{
+			currentNode++;
+			if (currentNode < mPath.size())
+				target = sf::Vector2f(mPath[currentNode].first.x * mTileSize, mPath[currentNode].first.y * mTileSize);
+			else
+			{
+				mGoalReached = true;
+				return;
+			}
+		}
+		float ang = atan2(target.y - mLocation.y, target.x - mLocation.x);
+		mAccel = sf::Vector2f(cos(ang), sin(ang)) * 200.f;
+
+		float diff = mPath[currentNode].second - mPath[currentNode - 1].second;
+		diff += (diff > pi) ? -(pi * 2) : (diff < -pi) ? (pi * 2) : 0;
+		float turnSpeed = 1;
+		if (fabs(diff) >= (pi / 2))
+			turnSpeed = .6f;
+		if (fabs(diff) >= (pi / 4))
+			turnSpeed = .8f;
+		mSpeed = max(min((ManhDist(mLocation.x, mLocation.y, target.x, target.y)) / 600, 1.f), turnSpeed);
+
+		//mVelocity = sf::Vector2f();
+
+		Entity::Update(t, offset, scale);
+	}
+}
+
+void Enemy::SetPath(std::vector<std::pair<sf::Vector2i, float>> p)
+{
+	mPath = p;
+	float minDist = FLT_MAX;
+	currentNode = p.size() - 1;
+	for (int i = 1; i < p.size() - 1; i++)
+	{
+		sf::Vector2f target(mPath[i].first.x * mTileSize, mPath[i].first.y * mTileSize);
+		float dist = ManhDist(mLocation.x, mLocation.y, target.x, target.y);
+		if (dist < minDist)
+		{
+			minDist = dist;
+			currentNode = i + 1;
+			if (dist < (mTileSize - 1))
+				break;
+		}
+	}
+	mGoalReached = false;
 }
--- TowerD/Enemy.h
+++ TowerD/Enemy.h
@@ -1,6 +1,5 @@
 #ifndef ENEMY_H
 #define ENEMY_H
-#include "SFML/Graphics.hpp" 
 #include "Entity.h"	
 #include "Gun.h"	
 
@@ -8,11 +7,21 @@ class Enemy : public Entity
 {
 private:
 	Gun mGun;
+	std::vector<std::pair<sf::Vector2i, float>> mPath;
+	std::vector<sf::Vector2f*> mPlayers;
+	int currentNode;
+	bool mGoalReached = false;
 
 public:
-	Enemy(sf::Vector2f, int, Renderer*);
-	void Update(float, sf::Vector2f);
+	Enemy(sf::Vector2f, int, Renderer*, std::vector<sf::Vector2f*>);
+	void Update(float, sf::Vector2f, float);
+	void Draw(sf::Vector2f, float);
 	void LoadAssets();
+	void SetPath(std::vector<std::pair<sf::Vector2i, float>>);
+	void Shoot(sf::Vector2f, float);
+	Gun* GetGun();
+	bool GoalReached();
+	~Enemy();
 };
 
 #endif
